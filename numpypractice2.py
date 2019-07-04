e30bf5cf0c7ed78bce895f6c9e2de467482c37b0
unknown <vidya.chitragar@lucidatechnologies.com>
2019-07-04 11:06:27 +0530

    changes are committed successfully

--- numpypractice2.py
+++ numpypractice2.py
@@ -78,23 +78,23 @@ import numpy as np
 # =============================================================================
 
 # =============================================================================
-# #Let us assume that we want to solve this linear equation set:
-# 
-# #x + 2*y = 8
-# #3*x + 4*y = 18
-# #This problem can be solved using linalg.solve method as shown in example below:
-# 
-# #filter_none
-# #edit
-# #play_arrow
-# 
-# #brightness_4
-# # coefficients 
-# a = np.array([[1, 2], [3, 4]]) 
-# # constants 
-# b = np.array([8, 18]) 
-#   
-# print("Solution of linear equations:", np.linalg.solve(a, b))
+#Let us assume that we want to solve this linear equation set:
+ 
+#x + 2*y = 8
+#3*x + 4*y = 18
+#This problem can be solved using linalg.solve method as shown in example below:
+ 
+#filter_none
+#edit
+#play_arrow
+ 
+#brightness_4
+# coefficients 
+a = np.array([[1, 2], [3, 4]]) 
+# constants 
+b = np.array([8, 18]) 
+   
+print("Solution of linear equations:", np.linalg.solve(a, b))
 # =============================================================================
 import matplotlib.pyplot as plt 
   
